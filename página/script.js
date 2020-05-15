3a51fe7d3db8338aedd0930d5ea2e5934d149551
brunosoharagit <b.sohara.git@gmail.com>
2020-05-15 12:13:46 -0300

    página's first commit

---
+++ página/script.js
@@ -0,0 +1,20 @@
+function pressButton()
+{
+    document.getElementById("thanks").innerHTML = "Eae mano"
+    document.getElementById("text").innerHTML = "Bem-vindo"
+}
+
+function redirect()
+{
+    window.open("http://services.runescape.com/m=clan-home/l=3/clan/Solaria")
+}
+
+function load()
+{
+    alert("Página carregada")
+}
+
+function change(element)
+{
+    console.log(element.value)
+}
